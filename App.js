ac789878575d55cf6cd1c0d5ef8b6939781157e3
Coby Walsh <cobywalsh7@yahoo.com>
2025-02-12 15:28:33 -0700

    working on getting the login page to render

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import { Admin, Resource } from 'react-admin';
 import authProvider from "./authProvider";
 import LoginPage from "./loginPage";
 import dataProvider from './dataProvider';
+import ReactDom from 'react-dom';
+
+ReactDom.render (
+    <App />,
+    document.getElementById('root')
+);
 
 const App = () => (
     <Admin authProvider={authProvider} loginPage={LoginPage} dataProvider={dataProvider}>
